831d6184da0b1e0691d49ceba3cf018f50e739df
Kurama <60360593+Kurama250@users.noreply.github.com>
2023-08-05 18:47:17 +0200

    Update and rename package.sh to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# package.sh by Kurama250
+# setup.sh by Kurama250
 # Github : https://github.com/Kurama250
 
 apt update && apt upgrade -y
-apt install npm node.js zip -y
+apt install npm node.js git -y
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash - &&\
 apt-get install -y nodejs -y
+git clone https://github.com/Kurama250/Detect_ssh.git
+cd Detect_ssh/
 npm install fs child_process axios tail
 npm install pm2 -g
